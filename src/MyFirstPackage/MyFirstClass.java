6d85e76a34d8b595226436b5242a483e1143da13
Mohammad Rahman <mohammadrahman@mohammads-mbp.home>
2019-12-21 14:10:43 -0500

    First commit

---
+++ src/MyFirstPackage/MyFirstClass.java
@@ -0,0 +1,10 @@
+package MyFirstPackage ;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/basics/HelloWorld.java
@@ -0,0 +1,24 @@
+package basics;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        byte dogage = 12;
+
+        short myShort = 4423;
+
+        int accountBalance = 43234233;
+
+        long anotherAccountBalance = 37843249865L;
+
+        float averageAge = 33.22f;
+
+        double average = 88.99;
+
+         boolean isDoorClosed = true;
+
+         char firstInitial = 'M';
+    }
+}
+
